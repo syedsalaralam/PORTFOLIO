94649d018f8e5eed66dc22782db27e8720003986
syedsalaralam <syedsalaralam786@gmail.com>
2023-11-22 20:08:07 +0500

    first commit

--- src/app/components/compo2.tsx
+++ src/app/components/compo2.tsx
@@ -5,18 +5,17 @@ const Compo2 = () => {
         MY AIM
       </h1>
       <div>
-        {" "}
         <p className=" font-bold font-sans text-center mt-16 mx-5 text-2xl text-gray-600  ">
-          "To seamlessly merge my expertise in React, Next.js, and Tailwind CSS
-          with a deep dive into backend technologies, databases, and server-side
-          development.
+        &quot;To seamlessly merge my expertise in React, Next.js, and Tailwind CSS
+          with a deep dive into backend technologies, databases, and serverside
+          development.&quot;
           <br /> My goal is to become a well-rounded full stack developer,
           capable of creating end-to-end web applications that are not only
           visually stunning but also robust, efficient, and secure.
-          <br /> I'm committed to continuous learning and growth, with a focus
+          <br /> &quot;I&apos;m committed to continuous learning and growth, with a focus
           on mastering technologies like Node.js and various databases, to
           deliver exceptional and complete digital solutions for a diverse range
-          of projects."
+          of projects.&quot;
         </p>
       </div>
     </div>
